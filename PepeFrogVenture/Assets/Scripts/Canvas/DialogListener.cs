19142c54fab489ca5830a9b045cde74f505aecde
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-04-22 13:50:20 +0200

    Lilypads som försvinner

---
+++ PepeFrogVenture/Assets/Scripts/Canvas/DialogListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Callback;
+
+public class DialogListener : MonoBehaviour
+{
+    [SerializeField] private GameObject Text;
+    void Start()
+    {
+        EventSystem.Current.RegisterListener<NPCDialogueEvent>(UpdateText);
+        Text.SetActive(false);
+
+    }
+
+    public void UpdateText(NPCDialogueEvent e)
+    {
+        Text.SetActive(true);
+        Text.GetComponent<Text>().text = e.Text;
+
+        Invoke("RemoveText", 8);
+    }
+
+    public void RemoveText()
+    {
+        Text.SetActive(false);
+    }
+}
---
+++ PepeFrogVenture/Assets/Scripts/EventSystem/EventTypes/NPCDialogueEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Callback
+{
+    public class NPCDialogueEvent : Event
+    {
+        public string Text;
+        
+        public NPCDialogueEvent(string newText)
+        {
+            Text = newText;
+        }
+    }
+}
