e15440c49ba985e28531e2d2df38df04d3976007
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-22 15:12:47 +0200

    ges

--- PepeFrogVenture/Assets/Scripts/Boss/BossProjectile.cs
+++ PepeFrogVenture/Assets/Scripts/Boss/BossProjectile.cs
@@ -5,27 +5,30 @@ using Callback;
 
 public class BossProjectile : MonoBehaviour
 {
-    private float speed;
-    [SerializeField] private float projectileSpeed;
-    public Vector3 target;
-    private Vector3 start;
-    [SerializeField] private float projectileHeight;
+    //private float speed;
+    //[SerializeField] private float projectileSpeed;
+    //public Vector3 target;
+    //private Vector3 start;
+    //[SerializeField] private float projectileHeight;
+    [SerializeField] private float projektileForce = 1000f;
     [SerializeField] private float damage = 4;
 
     private void Start()
     {
-        start = transform.position;
+        //start = transform.position;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * projektileForce);
         Destroy(gameObject, 5f);
     }
 
-    public void setTarget(Vector3 targetVector)
-    {
-        target = targetVector;
-    }
-    
+    //public void setTarget(Vector3 targetVector)
+    //{
+    //    target = targetVector;
+    //}
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             EventSystem.Current.FireEvent(new PlayerHitEvent(collision.gameObject, damage));
             //collision.gameObject.GetComponent<PlayerKontroller3D>().GetGameController().TakeDamage(damage);
@@ -33,21 +36,18 @@ public class BossProjectile : MonoBehaviour
         }
         Destroy(gameObject);
     }
-    void Update()
-    {
-        speed += Time.deltaTime;
+    //gamla sättet att flytta projektilen på
+    //void Update()
+    //{
+    //    speed += Time.deltaTime;
 
-        speed = speed % 5f;
+    //    speed = speed % 5f;
 
-        transform.position = MathParabola.Parabola(start, target, projectileHeight, speed / projectileSpeed);
-        if(Vector3.Distance(transform.position,target) < 0.1f)
-        {
-            Destroy(gameObject);
-        }
-    }
+    //    transform.position = MathParabola.Parabola(start, target, projectileHeight, speed / projectileSpeed);
+    //    if(Vector3.Distance(transform.position,target) < 0.1f)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 
-    private void Traveling()
-    {
-
-    }
 }
