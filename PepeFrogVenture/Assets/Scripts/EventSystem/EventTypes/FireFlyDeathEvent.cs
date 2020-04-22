4eac73b455d9061437bf00ff02aba0c5e6074921
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-04-22 14:52:53 +0200

    Början på ett bättre spawner system

---
+++ PepeFrogVenture/Assets/Scripts/EventSystem/EventTypes/FireFlyDeathEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Callback
+{
+    public class FireFlyDeathEvent : Event
+    {
+        public GameObject Parent;
+
+        public FireFlyDeathEvent(GameObject newParent)
+        {
+            Parent = newParent;
+        }
+    }
+}
