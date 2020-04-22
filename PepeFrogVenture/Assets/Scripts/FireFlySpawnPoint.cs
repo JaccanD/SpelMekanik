dc7752f4ec429c9af57fb47cf350510fbe3520f7
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-22 16:07:51 +0200

    fireflyspawnpoint

---
+++ PepeFrogVenture/Assets/Scripts/FireFlySpawnPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlySpawnPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject fireFlyPreFab;
+    private GameObject currentFirefly;
+
+    [SerializeField] private float spawnTimer = 5f;
+    private float currentSpawnTimeLeft = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentFirefly == null)
+        {
+            currentSpawnTimeLeft += Time.deltaTime;
+            if(currentSpawnTimeLeft > spawnTimer)
+            {
+                currentFirefly = Instantiate(fireFlyPreFab, transform.position, Quaternion.identity);
+                currentSpawnTimeLeft = 0;
+            }
+        }
+    }
+}
