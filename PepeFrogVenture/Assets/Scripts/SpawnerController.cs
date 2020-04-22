58bfcd650d6309e80e70830abeedde175332a306
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-04-22 15:30:26 +0200

    En bra Firefly spawner för bossen

--- PepeFrogVenture/Assets/Scripts/SpawnerController.cs
+++ PepeFrogVenture/Assets/Scripts/SpawnerController.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Callback;
 
-public class SpawnController : MonoBehaviour
+public class SpawnerController : MonoBehaviour
 {
+    [SerializeField] private GameObject FireFlyPrefab;
     [SerializeField] private int MaxChildren;
     private int CurrentChildren;
 
     [SerializeField] private GameObject[] Spawners;
-    private bool[] IsOccupied;
-
-
+    private Dictionary<GameObject, bool> IsOccupied;
 
     [SerializeField] private float RespawnDelay;
     private float Timer = 0;
 
     private void Start()
     {
-        IsOccupied = new bool[Spawners.Length];
-        for(int i = 0; i < Spawners.Length; i++)
+        IsOccupied = new Dictionary<GameObject, bool>();
+        foreach(GameObject s in Spawners)
         {
-            IsOccupied[i] = false;
+            IsOccupied[s] = false;
         }
 
         //Registrera Lyssnare för när en EldFluga Äts Upp
@@ -36,13 +35,23 @@ public class SpawnController : MonoBehaviour
         if (Timer < RespawnDelay)
             return;
 
-
+        foreach(GameObject s in Spawners)
+        {
+            if (!IsOccupied[s])
+            {
+                GameObject newFlies = Instantiate(FireFlyPrefab, s.transform.position + Vector3.up * 2, transform.rotation);
+                newFlies.GetComponent<FireFlyOnDestroy>().Parent = s;
+                IsOccupied[s] = true;
+                CurrentChildren++;
+                Timer = 0;
+                break;
+            }
+        }
     }
 
     public void OnEatEvent(FireFlyDeathEvent e)
     {
-        //Få vilken parent flugan hade från eventet
-        // Hitta parenten i Spawners
-        // Sätt den platsen i IsOccupied till false;
+        IsOccupied[e.Parent] = false;
+        CurrentChildren--;
     }
 }
