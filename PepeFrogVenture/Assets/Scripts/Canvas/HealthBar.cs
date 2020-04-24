8bcaa9c6a68498ee3d34d92a9f4f35ed55873c0a
Derfava <agge1610@gmail.com>
2020-04-24 11:35:36 +0200

    pls wörk

--- PepeFrogVenture/Assets/Scripts/Canvas/HealthBar.cs
+++ PepeFrogVenture/Assets/Scripts/Canvas/HealthBar.cs
@@ -7,14 +7,6 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
-<<<<<<< HEAD
-=======
-    public void SetMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
->>>>>>> parent of 0532aa8... tdh
     public void SetHealth(int health)
     {
         slider.value = health;
