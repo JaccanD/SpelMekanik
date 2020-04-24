2d5ad19d78887bdbdaa03171e46f9ea4edcd3dab
Derfava <agge1610@gmail.com>
2020-04-24 15:31:57 +0200

    blueberrycounter lol

---
+++ PepeFrogVenture/Assets/Scripts/Canvas/BlueBerryCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Callback;
+
+public class BlueBerryCounter : MonoBehaviour
+{
+    public Text BlueBerryCount;
+    private int BlueBerryInt = 0;
+
+    private void Start()
+    {
+        EventSystem.Current.RegisterListener(typeof(PickupEvent), GainBlueBerry);
+    }
+
+    public void GainBlueBerry(Callback.Event eb)
+    {
+        PickupEvent e = (PickupEvent)eb;
+
+        if (e.Pickup.tag == "Berry")
+        {
+            BlueBerryInt++;
+            BlueBerryCount.text = BlueBerryInt.ToString();
+        }
+    }
+}
