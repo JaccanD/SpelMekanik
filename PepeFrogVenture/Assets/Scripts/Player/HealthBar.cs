c15e0bfa27d863898a955d43a32ffe675b63baf6
Derfava <agge1610@gmail.com>
2020-04-24 11:19:54 +0200

    awa

--- PepeFrogVenture/Assets/Scripts/Player/HealthBar.cs
+++ PepeFrogVenture/Assets/Scripts/Player/HealthBar.cs
@@ -7,11 +7,6 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
-    public void SetMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
     public void SetHealth(int health)
     {
         slider.value = health;
