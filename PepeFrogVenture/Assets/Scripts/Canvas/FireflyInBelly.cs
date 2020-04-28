dfd430a02aac04118071bfa80b63bb2adcd7e851
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-28 16:26:44 +0200

    flygor roamar nu

--- PepeFrogVenture/Assets/Scripts/Canvas/FireflyInBelly.cs
+++ PepeFrogVenture/Assets/Scripts/Canvas/FireflyInBelly.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireflyInBelly : MonoBehaviour
-{
-    public ParticleSystem BellyGlow;
+//public class FireflyInBelly : MonoBehaviour
+//{
+//    public ParticleSystem BellyGlow;
 
-    private void Start()
-    {
-        EventSystem.Current.RegisterListener(typeof(PickupEvent), FireBelly);
-        EventSystem.Current.RegisterListener(typeof(FireballshotEvent), LoseFireBelly)
-    }
+//    private void Start()
+//    {
+//        EventSystem.Current.RegisterListener(typeof(PickupEvent), FireBelly);
+//        EventSystem.Current.RegisterListener(typeof(FireballshotEvent), LoseFireBelly)
+//    }
 
-    public void FireBelly(Callback.Event eb)
-    {
-        PickupEvent e = (PickupEvent)eb;
+//    public void FireBelly(Callback.Event eb)
+//    {
+//        PickupEvent e = (PickupEvent)eb;
 
-        if (e.Pickup.tag == "Fire")
-        {
-            BellyGlow.enableEmission;
-        }
-    }
+//        if (e.Pickup.tag == "Fire")
+//        {
+//            BellyGlow.enableEmission;
+//        }
+//    }
 
-    public void LoseFireBelly(Callback.Event eb)
-    {
-        QuestDoneEvent e = (QuestDoneEvent)eb;
+//    public void LoseFireBelly(Callback.Event eb)
+//    {
+//        QuestDoneEvent e = (QuestDoneEvent)eb;
 
-        BlueBerryInt = 0;
-        BlueBerryCount.text = BlueBerryInt.ToString();
-    }
-}
+//        BlueBerryInt = 0;
+//        BlueBerryCount.text = BlueBerryInt.ToString();
+//    }
+//}
