42347669356d4d06eea0314dd1e429d66733cb26
Derfava <agge1610@gmail.com>
2020-04-28 15:39:53 +0200

    Lite kodskit

--- PepeFrogVenture/Assets/Scripts/Canvas/FireflyInBelly.cs
+++ PepeFrogVenture/Assets/Scripts/Canvas/FireflyInBelly.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FireflyInBelly : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ParticleSystem BellyGlow;
+
+    private void Start()
     {
-        
+        EventSystem.Current.RegisterListener(typeof(PickupEvent), FireBelly);
+        EventSystem.Current.RegisterListener(typeof(FireballshotEvent), LoseFireBelly)
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FireBelly(Callback.Event eb)
     {
-        
+        PickupEvent e = (PickupEvent)eb;
+
+        if (e.Pickup.tag == "Fire")
+        {
+            BellyGlow.enableEmission;
+        }
+    }
+
+    public void LoseFireBelly(Callback.Event eb)
+    {
+        QuestDoneEvent e = (QuestDoneEvent)eb;
+
+        BlueBerryInt = 0;
+        BlueBerryCount.text = BlueBerryInt.ToString();
     }
 }
---
+++ PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlyRoaming : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 moveToPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        moveToPosition = Random.insideUnitSphere * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (moveToPosition - startPosition) * Time.deltaTime;
+    }
+}
