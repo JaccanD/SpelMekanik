f967c8992de77abc8e2ab286e7a8dd972b324944
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-28 15:13:39 +0200

    gh

---
+++ PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlyRoaming : MonoBehaviour
+{
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
