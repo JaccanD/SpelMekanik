ed02d9c4041b90b1dc4b742219fa07e023694510
Derfava <agge1610@gmail.com>
2020-04-28 17:20:22 +0200

    pls wörk

---
+++ PepeFrogVenture/Assets/Scripts/Canvas/FireReady.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Callback;
+
+public class FireReady : MonoBehaviour
+{
+    public Image fireImage;
+
+    private void Start()
+    {
+        fireImage.enabled = false;
+        EventSystem.Current.RegisterListener(typeof(PickupEvent), FireBelly);
+        EventSystem.Current.RegisterListener(typeof(FireballshotEvent), LoseFireBelly);
+        EventSystem.Current.RegisterListener(typeof(PlayerDeathEvent), LoseFireBellyonDeath);
+    }
+
+    public void FireBelly(Callback.Event eb)
+    {
+        PickupEvent e = (PickupEvent)eb;
+
+        if (e.Pickup.tag == "Fire")
+        {
+            fireImage.enabled = true;
+        }
+    }
+
+    public void LoseFireBelly(Callback.Event eb)
+    {
+        fireImage.enabled = false;
+    }
+
+    public void LoseFireBellyonDeath(Callback.Event eb)
+    {
+        fireImage.enabled = false;
+    }
+}
