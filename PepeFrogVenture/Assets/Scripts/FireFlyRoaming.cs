53beed5db2d3d0d9a16d0f98b7ccf0c4ecfcde59
Derfava <agge1610@gmail.com>
2020-04-28 16:35:22 +0200

    happ

--- PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
+++ PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
@@ -6,16 +6,27 @@ public class FireFlyRoaming : MonoBehaviour
 {
     private Vector3 startPosition;
     private Vector3 moveToPosition;
+    private Vector3 lastPosition;
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float radius = 2;
+    
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
-        moveToPosition = Random.insideUnitSphere * 5;
+        lastPosition = transform.position;
+        moveToPosition = Random.insideUnitSphere * radius + startPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += (moveToPosition - startPosition) * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPosition, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, moveToPosition) < 0.2f)
+        {
+            Debug.Log("changing position");
+            moveToPosition = Random.insideUnitSphere * radius + startPosition;
+            lastPosition = transform.position;
+        }
     }
 }
---
+++ PepeFrogVenture/Assets/Scripts/Player/PlayerSound/PlayerSoundScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Callback;
+
+public class PlayerSoundScript : MonoBehaviour
+{
+    [SerializeField] private AudioSource PlayerAudioSource;
+
+    private void Start()
+    {
+        
+    }
+}
