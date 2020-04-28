68549a40fb931017e94345e030decdd35203198f
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-28 15:26:27 +0200

    sfd

--- PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
+++ PepeFrogVenture/Assets/Scripts/FireFlyRoaming.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class FireFlyRoaming : MonoBehaviour
 {
     private Vector3 startPosition;
+    private Vector3 moveToPosition;
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        moveToPosition = Random.insideUnitSphere * 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (moveToPosition - startPosition) * Time.deltaTime;
     }
 }
