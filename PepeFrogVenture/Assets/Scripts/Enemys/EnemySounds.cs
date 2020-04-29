c67ecbfa29b1d437ca5b9abb4afe95ade2424fab
Jackaan <62544231+Jackaan@users.noreply.github.com>
2020-04-29 16:18:37 +0200

    stampad råtta ljud

--- PepeFrogVenture/Assets/Scripts/Enemys/EnemySounds.cs
+++ PepeFrogVenture/Assets/Scripts/Enemys/EnemySounds.cs
@@ -8,12 +8,14 @@ public class EnemySounds : MonoBehaviour
     // hej proggare. Denna klass har Jag(Jack) gjort så om något är tokigt, feel free att göra vad ni vill med koden nedan!
 
     [SerializeField] private AudioClip EnemyDead;
+    [SerializeField] private AudioClip FlatRat;
 
 
     // Start is called before the first frame update
     void Start()
     {
         EventSystem.Current.RegisterListener(typeof(EnemyHitEvent), OnEnemyHit);
+        EventSystem.Current.RegisterListener(typeof(EnemyStompedEvent), OnEnemyStomp);
     }
 
     public void OnEnemyHit(Callback.Event eb) 
@@ -27,5 +29,15 @@ public class EnemySounds : MonoBehaviour
 
         AudioSource.PlayClipAtPoint(EnemyDead, transform.position);
         EventSystem.Current.UnRegisterListener(typeof(EnemyHitEvent), OnEnemyHit);
+    }
+
+    public void OnEnemyStomp(Callback.Event eb)
+    {
+        EnemyStompedEvent e = (EnemyStompedEvent)eb;
+        if (e.enemyStomped != gameObject)
+            return;
+
+        AudioSource.PlayClipAtPoint(FlatRat, transform.position);
+        EventSystem.Current.UnRegisterListener(typeof(EnemyStompedEvent), OnEnemyStomp);
     }
 }
