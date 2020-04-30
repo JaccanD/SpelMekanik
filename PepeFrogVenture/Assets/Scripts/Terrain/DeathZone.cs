9e7cfd3d3d386112441474b72b4a4d9fe3648e37
Derfava <agge1610@gmail.com>
2020-04-30 14:57:21 +0200

    Jacob pls

--- PepeFrogVenture/Assets/Scripts/Terrain/DeathZone.cs
+++ PepeFrogVenture/Assets/Scripts/Terrain/DeathZone.cs
@@ -6,6 +6,9 @@ using Callback;
 public class DeathZone : MonoBehaviour
 {
     BoxCollider coll;
+    [SerializeField]private ParticleSystem waterSplash;
+    private float timer = 0;
+
     public void Awake()
     {
         coll = GetComponent<BoxCollider>();
@@ -16,11 +19,14 @@ public class DeathZone : MonoBehaviour
         Collider[] overLaps = Physics.OverlapBox(transform.position + coll.center, coll.bounds.extents, transform.rotation);
         for (int i = 0; i < overLaps.Length; i++)
         {
-            if (overLaps[i].transform.gameObject.tag == "Player")
+            if (overLaps[i].transform.gameObject.tag == "Player" && timer <= 0)
             {
+                timer = 1;
+                GameObject.Instantiate(waterSplash, overLaps[i].transform); 
                 EventSystem.Current.FireEvent(new PlayerDeathEvent(overLaps[i].transform.gameObject));
                 break;
             }
+            timer -= Time.deltaTime;
         }
     }
 }
