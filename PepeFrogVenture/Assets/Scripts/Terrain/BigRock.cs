96373acfe431808a65a005d54514005e24504927
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-04-30 16:29:20 +0200

    bigrock

---
+++ PepeFrogVenture/Assets/Scripts/Terrain/BigRock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Callback;
+
+public class BigRock : MonoBehaviour
+{
+    [SerializeField] private GameObject NPCblocker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.Current.RegisterListener(typeof(QuestDoneEvent), BLASTOFF);
+    }
+
+    private void BLASTOFF(Callback.Event eb)
+    {
+        QuestDoneEvent e = (QuestDoneEvent)eb;
+        if(e.NPC != NPCblocker)
+        {
+            return;
+        }
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 4000);
+        Destroy(gameObject, 10f);
+    }
+}
