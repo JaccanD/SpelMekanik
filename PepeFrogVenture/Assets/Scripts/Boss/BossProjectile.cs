f2ee639634161c7039a5c01ee2a5c3dda9836150
Derfava <agge1610@gmail.com>
2020-05-05 11:46:05 +0200

    Partikeleffekter bossprojektil

--- PepeFrogVenture/Assets/Scripts/Boss/BossProjectile.cs
+++ PepeFrogVenture/Assets/Scripts/Boss/BossProjectile.cs
@@ -8,6 +8,7 @@ public class BossProjectile : MonoBehaviour
 {
     private float projectileForce;
     private float projectileDamage = 3;
+    [SerializeField] private ParticleSystem bossProjectileSplash;
 
     private void Start()
     {
@@ -21,6 +22,7 @@ public class BossProjectile : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            GameObject.Instantiate(bossProjectileSplash, transform.position, transform.rotation);
             EventSystem.Current.FireEvent(new PlayerHitEvent(collision.gameObject, projectileDamage));
         }
         Destroy(gameObject);
