ba2d0670cdb2b827fa7bc6f5277cca45fd7839ee
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-05-11 16:26:13 +0200

    Sikte

---
+++ PepeFrogVenture/Assets/Scripts/Canvas/CrossHairScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHairScript : MonoBehaviour
+{
+    // Cameran
+    // Tungländ
+
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float toungeLength;
+    [SerializeField] LayerMask hookMask;
+    Image crossHair;
+
+    [Header ("Cross Hair Colors")]
+    [SerializeField] Color missColor;
+    [SerializeField] Color hitColor;
+
+    private void Start()
+    {
+        crossHair = GetComponent<Image>();
+        crossHair.color = missColor;
+    }
+    private void Update()
+    {
+        bool aimHit = Physics.SphereCast(cameraTransform.position, 0.3f, cameraTransform.rotation * new Vector3(0, 0, 1), out RaycastHit HookCast, toungeLength, hookMask);
+
+        if (aimHit)
+        {
+            crossHair.color = hitColor;
+        }
+        else
+        {
+            crossHair.color = missColor;
+        }
+    }
+}
