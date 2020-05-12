d661db4e7001137f80286d8183df99f911a4ab52
qwertypops <63237367+qwertypops@users.noreply.github.com>
2020-05-12 11:59:46 +0200

    fireflies i scen två

--- PepeFrogVenture/Assets/Scripts/FireFlySpawner.cs
+++ PepeFrogVenture/Assets/Scripts/FireFlySpawner.cs
@@ -5,70 +5,70 @@ using UnityEngine;
 // Används inte
 public class FireFlySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject FireFly;
-    [SerializeField] private float SpawnDelay;
-    [SerializeField] private int MaxNumberOfFireFlies;
-    [SerializeField] private float Radius;
+    [SerializeField] private GameObject fireFly;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private int maxNumberOfFireFlies;
+    [SerializeField] private float radius;
 
-    private float TimeSinceLastSpawn;
-    private int CurrentNumberOfFireFlies;
-    private GameObject[] SpawnedFlies;
+    private float timeSinceLastSpawn;
+    private int currentNumberOfFireFlies;
+    private GameObject[] spawnedFlies;
 
     private void Update()
     {
-        if (CurrentNumberOfFireFlies < MaxNumberOfFireFlies && TimeSinceLastSpawn >= SpawnDelay)
+        if (currentNumberOfFireFlies < maxNumberOfFireFlies && timeSinceLastSpawn >= spawnDelay)
         {
             Vector3 position = new Vector3(1, 1, 1);
             Vector2 randomPos = Random.insideUnitCircle;
-            position.x *= randomPos.x * Radius;
-            position.z *= randomPos.y * Radius;
+            position.x *= randomPos.x * radius;
+            position.z *= randomPos.y * radius;
 
             position += transform.position;
 
-            GameObject newFireFly = Instantiate(FireFly, position, transform.rotation);
-            SpawnedFlies[CurrentNumberOfFireFlies] = newFireFly;
+            GameObject newFireFly = Instantiate(fireFly, position, transform.rotation);
+            spawnedFlies[currentNumberOfFireFlies] = newFireFly;
 
-            CurrentNumberOfFireFlies++;
-            TimeSinceLastSpawn = 0;
+            currentNumberOfFireFlies++;
+            timeSinceLastSpawn = 0;
             //Debug.Log(CurrentNumberOfFireFlies);
         }
-        TimeSinceLastSpawn += Time.deltaTime;
+        timeSinceLastSpawn += Time.deltaTime;
 
 
         int test = 0;
-        for(int i = 0; i < SpawnedFlies.Length; i++)
+        for(int i = 0; i < spawnedFlies.Length; i++)
         {
 
             int FireFlies = 0;
-            if(SpawnedFlies[i] != null)
+            if(spawnedFlies[i] != null)
             {
                 FireFlies++;
                 test++;
             }
-            if(FireFlies == MaxNumberOfFireFlies)
+            if(FireFlies == maxNumberOfFireFlies)
             {
-                TimeSinceLastSpawn = 0;
+                timeSinceLastSpawn = 0;
             }
         }
-        CurrentNumberOfFireFlies = test;
+        currentNumberOfFireFlies = test;
 
-        for(int i = 0; i < SpawnedFlies.Length - 1; i++)
+        for(int i = 0; i < spawnedFlies.Length - 1; i++)
         {
-            if(SpawnedFlies[i] == null)
+            if(spawnedFlies[i] == null)
             {
-                SpawnedFlies[i] = SpawnedFlies[i + 1];
-                SpawnedFlies[i + 1] = null;
+                spawnedFlies[i] = spawnedFlies[i + 1];
+                spawnedFlies[i + 1] = null;
             }
         }
-        if(SpawnedFlies[MaxNumberOfFireFlies - 1])
+        if(spawnedFlies[maxNumberOfFireFlies - 1])
         {
-            TimeSinceLastSpawn = 0;
+            timeSinceLastSpawn = 0;
         }
     }
     
     private void Awake()
     {
-        SpawnedFlies = new GameObject[MaxNumberOfFireFlies];
+        spawnedFlies = new GameObject[maxNumberOfFireFlies];
     }
 
 }
