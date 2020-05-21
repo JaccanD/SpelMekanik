9b0439aa6f1a48a3d759acd6babca25b512936cf
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-05-21 11:51:21 +0200

    checkbox för att använda control

---
+++ PepeFrogVenture/Assets/Scripts/Canvas/UseControllerCheckbox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseControllerCheckbox : MonoBehaviour
+{
+    Toggle button;
+
+    private void Awake()
+    {
+        button = GetComponent<Toggle>();
+        button.onValueChanged.AddListener(delegate {
+            SetUsingController(button);
+
+        });
+
+        button.isOn = Controlls.UsingController;
+    }
+
+    void SetUsingController(Toggle changed)
+    {
+        Controlls.UsingController = changed.isOn;
+    }
+    
+}
