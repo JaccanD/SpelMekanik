caff0f7655549be909f11952de6d736a81908593
Derfava <agge1610@gmail.com>
2020-05-26 16:04:18 +0200

    Animerade Npc:er

---
+++ PepeFrogVenture/Assets/NpcMotionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Callback;
+
+public class NpcMotionController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        EventSystem.Current.RegisterListener(typeof(QuestDoneEvent), Thank);
+    }
+
+    public void Thank(Callback.Event eb)
+    {
+        anim.SetTrigger("Thank");
+    }
+}
