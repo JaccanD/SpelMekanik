8184d728bc508688cbd02818f0a0426fa072288c
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-05-26 11:08:02 +0200

    Update SelectedMarker.cs

--- PepeFrogVenture/Assets/Scripts/Canvas/SelectedMarker.cs
+++ PepeFrogVenture/Assets/Scripts/Canvas/SelectedMarker.cs
@@ -24,6 +24,6 @@ public class SelectedMarker : MonoBehaviour
 
     private void MoveMarker()
     {
-        transform.position = selected.transform.position + Vector3.right * 2;
+        transform.position = selected.transform.position + Vector3.left * (2 + selected.GetComponent<RectTransform>().sizeDelta.x);
     }
 }
