7e8a55915ae94372bd1b910c1ea5c9f9678e5d83
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-05-27 10:55:56 +0200

    rikta spelaren när den  respawmar

--- PepeFrogVenture/Assets/Scripts/EventSystem/EventTypes/PlayerEvents/PlayerRespawnEvent.cs
+++ PepeFrogVenture/Assets/Scripts/EventSystem/EventTypes/PlayerEvents/PlayerRespawnEvent.cs
@@ -1,8 +1,19 @@
-﻿namespace Callback
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Callback;
+
+
+namespace Callback
 {
     public class PlayerRespawnEvent : Event
     {
+        public GameObject RespawnPoint;
 
+        public PlayerRespawnEvent(GameObject respawn)
+        {
+            RespawnPoint = respawn;
+        }
     }
 }
 
