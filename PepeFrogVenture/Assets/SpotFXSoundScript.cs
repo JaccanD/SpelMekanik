e32c77731d3fb7a7879de6651dc23e932be1c196
Jackaan <62544231+Jackaan@users.noreply.github.com>
2020-05-29 10:31:58 +0200

    SpotFX, crickets and börds

---
+++ PepeFrogVenture/Assets/SpotFXSoundScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotFXSoundScript : MonoBehaviour
+{
+    [SerializeField] private float MinDelay;
+    [SerializeField] private float MaxDelay;
+    [SerializeField] private AudioClip SpotFX;
+
+    private AudioSource source;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = SpotFX;
+    }
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            float d = Random.Range(MinDelay, MaxDelay);
+
+            source.PlayDelayed(d);
+        }
+    }
+}
