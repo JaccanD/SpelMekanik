1eaf7228b8a96abd2069576ab5329b58bf20bad6
Jackaan <62544231+Jackaan@users.noreply.github.com>
2020-06-01 14:13:56 +0200

    ljud lvl 2

--- PepeFrogVenture/Assets/Scripts/Boss/BossSoundScript.cs
+++ PepeFrogVenture/Assets/Scripts/Boss/BossSoundScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Callback;
 
-// Author: Valter Fallsterljung?
+// Author: Valter Fallsterljung? 
+// Jack (Designer) har också varit här
 public class BossSoundScript : MonoBehaviour
 {
 
@@ -12,14 +13,42 @@ public class BossSoundScript : MonoBehaviour
 
     [Header("Sounds")]
     [SerializeField] private AudioClip bossSpit;
+    [SerializeField] private AudioClip bossRapidAttack;
+    [SerializeField] private AudioClip bossSuperAttack;
+    [SerializeField] private AudioClip bossChargeAttack;
+
+
 
     void Start()
     {
         EventSystem.Current.RegisterListener(typeof(BossShootingEvent), OnBossSpit);
+        EventSystem.Current.RegisterListener(typeof(BossRapidAttackEvent), OnBossRapidAttack);
+        EventSystem.Current.RegisterListener(typeof(BossSuperAttackEvent), OnBossSuperAttack);
+        EventSystem.Current.RegisterListener(typeof(BossChargeAttackEvent), OnBossChargeAttack);
+
+
     }
     public void OnBossSpit(Callback.Event eb)
     {
         bossSound.volume = 0.5f;
         bossSound.PlayOneShot(bossSpit);
+    }
+
+    public void OnBossRapidAttack(Callback.Event eb)
+    {
+        bossSound.volume = 0.5f;
+        bossSound.PlayOneShot(bossRapidAttack);
+    }
+
+    public void OnBossSuperAttack(Callback.Event eb)
+    {
+        bossSound.volume = 0.5f;
+        bossSound.PlayOneShot(bossSuperAttack);
+    }
+
+    public void OnBossChargeAttack(Callback.Event eb)
+    {
+        bossSound.volume = 0.5f;
+        bossSound.PlayOneShot(bossChargeAttack);
     }
 }
