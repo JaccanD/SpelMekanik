e3f84dac078d7aba3edf93233fba22ed9fdd7e25
JaccanD <63229159+JaccanD@users.noreply.github.com>
2020-06-03 14:17:43 +0200

    fisk trigger

---
+++ PepeFrogVenture/Assets/Scripts/Terrain/FishTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject fish;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            fish.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
